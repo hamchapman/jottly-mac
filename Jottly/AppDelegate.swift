9e51fa81d8b3e5893497abade5deed55dfa17189
Hamilton Chapman <hamchapman@gmail.com>
2017-06-11 22:16:33 +0100

    Renamed, tidied, and updated. Jottly.

--- Jottly/AppDelegate.swift
+++ Jottly/AppDelegate.swift
@@ -1,25 +1,17 @@
-//
-//  AppDelegate.swift
-//  MacPC
-//
-//  Created by Hamilton Chapman on 30/12/2015.
-//  Copyright © 2015 Hamilton Chapman. All rights reserved.
-//
-
 import Cocoa
 import CoreData
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
     let clicksMenuItem: NSMenuItem = NSMenuItem()
     let pressesMenuItem: NSMenuItem = NSMenuItem()
     let menu: NSMenu = NSMenu()
     
     @IBOutlet weak var window: NSWindow!
 
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
         if let button = statusItem.button {
             button.image = NSImage(named: "StatusBarButtonImage")
         }
@@ -32,6 +24,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func setMenuItems() {
         var clickTotal = 0
         var pressTotal = 0
+
         if let clicksToday = fetchTodaysTotalIfPresent("mouse") {
             clickTotal = clicksToday
         }
@@ -42,7 +35,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         pressesMenuItem.title = "Key presses today: \(pressTotal)"
     }
     
-    func resync(sender: AnyObject) {
+    func resync(_ sender: AnyObject) {
         print("About to resync")
     }
     
@@ -57,56 +50,61 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func setupMasks() {
-        NSEvent.addGlobalMonitorForEventsMatchingMask(.LeftMouseDownMask, handler: { event in
+        NSEvent.addGlobalMonitorForEvents(matching: .leftMouseDown, handler: { event in
             print("Left mouse click")
             self.incrementOrCreate("mouse")
         })
         
-        NSEvent.addGlobalMonitorForEventsMatchingMask(.RightMouseDownMask, handler: { event in
+        NSEvent.addGlobalMonitorForEvents(matching: .rightMouseDown, handler: { event in
             print("Right mouse click")
             self.incrementOrCreate("mouse")
         })
         
-        NSEvent.addGlobalMonitorForEventsMatchingMask(.KeyDownMask, handler: { event in
+        NSEvent.addGlobalMonitorForEvents(matching: .keyDown, handler: { event in
             print("Key press char:\(event.characters) key code: \(event.keyCode)")
+            guard ![123, 124].contains(event.keyCode) else {
+                // Left or right arrow presssed in conjunction with a modifier key
+                print("Not counted as a valid key press")
+                return
+            }
             self.incrementOrCreate("key")
         })
         
-        NSEvent.addGlobalMonitorForEventsMatchingMask(.FlagsChangedMask, handler: { event in
+        NSEvent.addGlobalMonitorForEvents(matching: .flagsChanged, handler: { event in
             if event.modifierFlags.rawValue == 256 {
                 print("Modifier key up")
                 self.incrementOrCreate("key")
             }
         })
     }
-    
+
     func setupMenu() {
         menu.addItem(clicksMenuItem)
         menu.addItem(pressesMenuItem)
-        menu.addItem(NSMenuItem(title: "Resync", action: Selector("resync:"), keyEquivalent: "R"))
-        menu.addItem(NSMenuItem.separatorItem())
-        menu.addItem(NSMenuItem(title: "Quit", action: Selector("terminate:"), keyEquivalent: "q"))
+        menu.addItem(NSMenuItem(title: "Resync", action: #selector(AppDelegate.resync(_:)), keyEquivalent: "R"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApp.terminate(_:)), keyEquivalent: "q"))
         statusItem.menu = menu
     }
     
     func todaysDateAsString() -> String {
-        let todaysDate = NSDate()
-        let dateFormatter = NSDateFormatter()
+        let todaysDate = Date()
+        let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd"
-        return dateFormatter.stringFromDate(todaysDate)
+        return dateFormatter.string(from: todaysDate)
     }
     
-    func fetchTodaysObjectIfPresent(type: String) -> NSManagedObject? {
+    func fetchTodaysObjectIfPresent(_ type: String) -> NSManagedObject? {
         let managedContext = self.managedObjectContext
         let dateString = todaysDateAsString()
         
-        let fetchRequest = NSFetchRequest(entityName: "DayTotal")
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "DayTotal")
         fetchRequest.predicate = NSPredicate(format: "createdAt = %@ AND type = %@", dateString, type)
         
         // maybe store todays managed object id in variable
         
         do {
-            if let results = try managedContext.executeFetchRequest(fetchRequest) as? [NSManagedObject] {
+            if let results = try managedContext.fetch(fetchRequest) as? [NSManagedObject] {
                 if results.count == 1 {
                     return results[0]
                 }
@@ -118,25 +116,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return nil
     }
     
-    func fetchTodaysTotalIfPresent(type: String) -> Int? {
+    func fetchTodaysTotalIfPresent(_ type: String) -> Int? {
         if let todaysObject = fetchTodaysObjectIfPresent(type) {
-            return todaysObject.valueForKey("total") as? Int
+            return todaysObject.value(forKey: "total") as? Int
         }
         return nil
     }
     
-    func incrementOrCreate(type: String) {
+    func incrementOrCreate(_ type: String) {
         let managedContext = self.managedObjectContext
         let dateString = todaysDateAsString()
         var newTotal: Int
         
         if let todaysObject = fetchTodaysObjectIfPresent(type) {
-            let currentTotal = todaysObject.valueForKey("total") as! Int
+            let currentTotal = todaysObject.value(forKey: "total") as! Int
             newTotal = currentTotal + 1
             todaysObject.setValue(newTotal, forKey: "total")
         } else {
-            let entity =  NSEntityDescription.entityForName("DayTotal", inManagedObjectContext: managedContext)
-            let dayTotal = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+            let entity =  NSEntityDescription.entity(forEntityName: "DayTotal", in: managedContext)
+            let dayTotal = NSManagedObject(entity: entity!, insertInto: managedContext)
             dayTotal.setValue(dateString, forKey: "createdAt")
             dayTotal.setValue(type, forKey: "type")
             newTotal = 1
@@ -146,7 +144,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         updateMenuTitle(type, value: newTotal)
     }
     
-    func updateMenuTitle(type: String, value: Int) {
+    func updateMenuTitle(_ type: String, value: Int) {
         if type == "key" {
             pressesMenuItem.title = "Key presses today: \(value)"
         } else if type == "mouse" {
@@ -155,55 +153,54 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
     
     func acquirePrivileges() -> Bool {
-        let trusted = kAXTrustedCheckOptionPrompt.takeUnretainedValue()
-        let privOptions = [trusted as NSString: true]
-        var accessEnabled = AXIsProcessTrustedWithOptions(privOptions)
-        
-        if !accessEnabled {
+        let options: NSDictionary = [kAXTrustedCheckOptionPrompt.takeRetainedValue() as NSString: true]
+        var accessibilityEnabled = AXIsProcessTrustedWithOptions(options)
+
+        guard accessibilityEnabled else {
             let alert = NSAlert()
-            alert.messageText = "Enable MacPC"
-            alert.informativeText = "Once you have enabled MacPC in System Preferences, click OK."
-            alert.beginSheetModalForWindow(self.window, completionHandler: { response in
-                if AXIsProcessTrustedWithOptions(privOptions) {
-                    accessEnabled = true
+            alert.messageText = "Enable Jottly"
+            alert.informativeText = "Once you have enabled Jottly in System Preferences, click OK."
+            alert.beginSheetModal(for: self.window, completionHandler: { response in
+                if AXIsProcessTrustedWithOptions(options as CFDictionary) {
+                    accessibilityEnabled = true
                 } else {
                     NSApp.terminate(self)
                 }
             })
+            return accessibilityEnabled
         }
-        return accessEnabled
+
+        return accessibilityEnabled
     }
 
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
+    func applicationWillTerminate(_ aNotification: Notification) {}
 
     // MARK: - Core Data stack
 
-    lazy var applicationDocumentsDirectory: NSURL = {
-        // The directory the application uses to store the Core Data store file. This code uses a directory named "gg.hc.MacPC" in the user's Application Support directory.
-        let urls = NSFileManager.defaultManager().URLsForDirectory(.ApplicationSupportDirectory, inDomains: .UserDomainMask)
+    lazy var applicationDocumentsDirectory: URL = {
+        // The directory the application uses to store the Core Data store file. This code uses a directory named "gg.hc.Jottly" in the user's Application Support directory.
+        let urls = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)
         let appSupportURL = urls[urls.count - 1]
-        return appSupportURL.URLByAppendingPathComponent("gg.hc.MacPC")
+        return appSupportURL.appendingPathComponent("gg.hc.Jottly")
     }()
 
     lazy var managedObjectModel: NSManagedObjectModel = {
         // The managed object model for the application. This property is not optional. It is a fatal error for the application not to be able to find and load its model.
-        let modelURL = NSBundle.mainBundle().URLForResource("MacPC", withExtension: "momd")!
-        return NSManagedObjectModel(contentsOfURL: modelURL)!
+        let modelURL = Bundle.main.url(forResource: "Jottly", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelURL)!
     }()
 
     lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
         // The persistent store coordinator for the application. This implementation creates and returns a coordinator, having added the store for the application to it. (The directory for the store is created, if necessary.) This property is optional since there are legitimate error conditions that could cause the creation of the store to fail.
-        let fileManager = NSFileManager.defaultManager()
+        let fileManager = FileManager.default
         var failError: NSError? = nil
         var shouldFail = false
         var failureReason = "There was an error creating or loading the application's saved data."
 
         // Make sure the application files directory is there
         do {
-            let properties = try self.applicationDocumentsDirectory.resourceValuesForKeys([NSURLIsDirectoryKey])
-            if !properties[NSURLIsDirectoryKey]!.boolValue {
+            let properties = try (self.applicationDocumentsDirectory as NSURL).resourceValues(forKeys: [URLResourceKey.isDirectoryKey])
+            if !(properties[URLResourceKey.isDirectoryKey]! as AnyObject).boolValue {
                 failureReason = "Expected a folder to store application data, found a file \(self.applicationDocumentsDirectory.path)."
                 shouldFail = true
             }
@@ -211,7 +208,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             let nserror = error as NSError
             if nserror.code == NSFileReadNoSuchFileError {
                 do {
-                    try fileManager.createDirectoryAtPath(self.applicationDocumentsDirectory.path!, withIntermediateDirectories: true, attributes: nil)
+                    try fileManager.createDirectory(atPath: self.applicationDocumentsDirectory.path, withIntermediateDirectories: true, attributes: nil)
                 } catch {
                     failError = nserror
                 }
@@ -224,9 +221,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         var coordinator: NSPersistentStoreCoordinator? = nil
         if failError == nil {
             coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
-            let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("CocoaAppCD.storedata")
+            let url = self.applicationDocumentsDirectory.appendingPathComponent("CocoaAppCD.storedata")
             do {
-                try coordinator!.addPersistentStoreWithType(NSXMLStoreType, configuration: nil, URL: url, options: nil)
+                try coordinator!.addPersistentStore(ofType: NSXMLStoreType, configurationName: nil, at: url, options: nil)
             } catch {
                 failError = error as NSError
             }
@@ -235,13 +232,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         if shouldFail || (failError != nil) {
             // Report any error we got.
             var dict = [String: AnyObject]()
-            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data"
-            dict[NSLocalizedFailureReasonErrorKey] = failureReason
+            dict[NSLocalizedDescriptionKey] = "Failed to initialize the application's saved data" as AnyObject
+            dict[NSLocalizedFailureReasonErrorKey] = failureReason as AnyObject
             if failError != nil {
                 dict[NSUnderlyingErrorKey] = failError
             }
             let error = NSError(domain: "YOUR_ERROR_DOMAIN", code: 9999, userInfo: dict)
-            NSApplication.sharedApplication().presentError(error)
+            NSApplication.shared().presentError(error)
             abort()
         } else {
             return coordinator!
@@ -251,43 +248,43 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     lazy var managedObjectContext: NSManagedObjectContext = {
         // Returns the managed object context for the application (which is already bound to the persistent store coordinator for the application.) This property is optional since there are legitimate error conditions that could cause the creation of the context to fail.
         let coordinator = self.persistentStoreCoordinator
-        var managedObjectContext = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        var managedObjectContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
         managedObjectContext.persistentStoreCoordinator = coordinator
         return managedObjectContext
     }()
 
     // MARK: - Core Data Saving and Undo support
 
-    @IBAction func saveAction(sender: AnyObject!) {
+    @IBAction func saveAction(_ sender: AnyObject!) {
         // Performs the save action for the application, which is to send the save: message to the application's managed object context. Any encountered errors are presented to the user.
         if !managedObjectContext.commitEditing() {
-            NSLog("\(NSStringFromClass(self.dynamicType)) unable to commit editing before saving")
+            NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing before saving")
         }
         if managedObjectContext.hasChanges {
             do {
                 try managedObjectContext.save()
             } catch {
                 let nserror = error as NSError
-                NSApplication.sharedApplication().presentError(nserror)
+                NSApplication.shared().presentError(nserror)
             }
         }
     }
 
-    func windowWillReturnUndoManager(window: NSWindow) -> NSUndoManager? {
+    func windowWillReturnUndoManager(_ window: NSWindow) -> UndoManager? {
         // Returns the NSUndoManager for the application. In this case, the manager returned is that of the managed object context for the application.
         return managedObjectContext.undoManager
     }
 
-    func applicationShouldTerminate(sender: NSApplication) -> NSApplicationTerminateReply {
+    func applicationShouldTerminate(_ sender: NSApplication) -> NSApplicationTerminateReply {
         // Save changes in the application's managed object context before the application terminates.
         
         if !managedObjectContext.commitEditing() {
-            NSLog("\(NSStringFromClass(self.dynamicType)) unable to commit editing to terminate")
-            return .TerminateCancel
+            NSLog("\(NSStringFromClass(type(of: self))) unable to commit editing to terminate")
+            return .terminateCancel
         }
         
         if !managedObjectContext.hasChanges {
-            return .TerminateNow
+            return .terminateNow
         }
         
         do {
@@ -296,8 +293,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             let nserror = error as NSError
             // Customize this code block to include application-specific recovery steps.
             let result = sender.presentError(nserror)
-            if (result) {
-                return .TerminateCancel
+            if result {
+                return .terminateCancel
             }
             
             let question = NSLocalizedString("Could not save changes while quitting. Quit anyway?", comment: "Quit without saves error question message")
@@ -307,16 +304,16 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             let alert = NSAlert()
             alert.messageText = question
             alert.informativeText = info
-            alert.addButtonWithTitle(quitButton)
-            alert.addButtonWithTitle(cancelButton)
+            alert.addButton(withTitle: quitButton)
+            alert.addButton(withTitle: cancelButton)
             
             let answer = alert.runModal()
             if answer == NSAlertFirstButtonReturn {
-                return .TerminateCancel
+                return .terminateCancel
             }
         }
         // If we got here, it is time to quit.
-        return .TerminateNow
+        return .terminateNow
     }
 
 }
